d31dc9f922b117f4b3b17c111ed3c1780a30a9fd
guilherme8l <165799977+UI-ONGs@users.noreply.github.com>
2024-10-15 09:08:19 -0300

    Aula - 15/10

--- app/src/main/java/guilherme/souza/MainActivity.java
+++ app/src/main/java/guilherme/souza/MainActivity.java
@@ -1,18 +1,23 @@
 package guilherme.souza;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import android.content.pm.PackageManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -21,6 +26,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int RESULT_REQUEST_PERMISSION = 2;
 
     BottomNavigationView bottomNavigationView;
     @Override
@@ -67,10 +74,80 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume(){
         super.onResume();
         List<String> permissions = new ArrayList<>();
-        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        permissions.add(Manifest.permission.
+                READ_EXTERNAL_STORAGE);
         checkForPermissions(permissions);
 
     }
+
+    // Método para verificar e solicitar permissões
+    private void checkForPermissions(List<String> permissions) {
+        List<String> permissionsNotGranted = new ArrayList<>();
+
+        for (String permission : permissions) {
+            if (!hasPermission(permission)) {
+                permissionsNotGranted.add(permission); // Adiciona à lista as permissões não concedidas
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permissionsNotGranted.size() > 0) {
+                // Solicita as permissões não concedidas
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
+            }
+            else{
+                MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+                int navigationOpSelected =vm.getNavigationOpSelected();
+                bottomNavigationView.setSelectedItemId(navigationOpSelected);
+            }
+        }
+    }
+
+    // Método para verificar se uma permissão foi concedida
+    private boolean hasPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    // Método que recebe o resultado da solicitação de permissões
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        final List<String> permissionsRejected = new ArrayList<>();
+        if (requestCode == RESULT_REQUEST_PERMISSION) {
+            for (String permission : permissions) {
+                if (!hasPermission(permission)) {
+                    permissionsRejected.add(permission); // Adiciona à lista as permissões rejeitadas
+                }
+            }
+        }
+
+        if (permissionsRejected.size() > 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                    // Mostra um alerta explicando a necessidade das permissões
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("Para usar esse app é preciso conceder essas permissões")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // Solicita novamente as permissões rejeitadas
+                                    requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
+                                }
+                            }).create().show();
+                }
+            }
+        }
+        else{
+            MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+            int navigationOpSelected = vm.getNavigationOpSelected();
+            bottomNavigationView.setSelectedItemId(navigationOpSelected);
+        }
+    }
+
 
 
 }
