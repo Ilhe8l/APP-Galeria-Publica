68ec2a7d24830c1bdb7faf337486caadb13cf08e
malu <maluarnholdsampaio@gmail.com>
2025-01-24 10:38:03 -0300

    Atualização Final - 24/01

--- app/src/main/java/guilherme/souza/MainActivity.java
+++ app/src/main/java/guilherme/souza/MainActivity.java
@@ -1,10 +1,13 @@
 package guilherme.souza;
 
-import android.Manifest;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.Manifest;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -17,19 +20,23 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
-import android.content.pm.PackageManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import guilherme.souza.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    //definicao do botao
+    BottomNavigationView bottomNavigationView;
+
     static int RESULT_REQUEST_PERMISSION = 2;
 
-    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,69 +48,81 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        checkForPermissions(permissions);
+
+        //obtendo a referencia do MainViewMode
         final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
 
+        //obtendo a referencia do BottonNavigationView
         bottomNavigationView = findViewById(R.id.btnNav);
-        bottomNavigationView.setOnItemReselectedListener(new NavigationBarView.OnItemSelectedListener() {
+        //escutador de cliques no btn
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            //apos clicar, essa funcao sera chamada e indicar a opcao que o usuario escolheu
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //guardamos a opcao indicada
                 vm.setNavigationOpSelected(item.getItemId());
-                switch (item.getItemId()){
-                    case R.id.gridViewOp:
-                        GridViewFragment gridViewFragment = GridViewFragment.newInstance();
-                        setFragment(gridViewFragment);
-                        break;
-                    case R.id.listViewOp:
-                        ListViewFragment listViewFragment = ListViewFragment.newInstance();
-                        setFragment(listViewFragment);
-                        break;
+                //aqui é um if/else que muda de acordo com a opcao
+                if (item.getItemId() == R.id.gridViewOp) {
+                    GridViewFragment gridViewFragment = GridViewFragment.newInstance();
+                    setFragment(gridViewFragment);
+                } else if (item.getItemId() == R.id.listViewOp) {
+                    ListViewFragment listViewFragment = ListViewFragment.newInstance();
+                    setFragment(listViewFragment);
                 }
                 return true;
             }
         });
+
+
     }
 
-    void setFragment(Fragment fragment){
+    // recebe de paramentro um fragment
+    void setFragment(Fragment fragment) {
+        // gerenciamento de fragmentos
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        // to setando esse parametro no elemento fragContainer
         fragmentTransaction.replace(R.id.fragContainer, fragment);
+        // o fragmento pertence ao botao voltar
         fragmentTransaction.addToBackStack(null);
+        // commit
         fragmentTransaction.commit();
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
-        List<String> permissions = new ArrayList<>();
-        permissions.add(Manifest.permission.
-                READ_EXTERNAL_STORAGE);
-        checkForPermissions(permissions);
 
     }
 
-    // Método para verificar e solicitar permissões
     private void checkForPermissions(List<String> permissions) {
-        List<String> permissionsNotGranted = new ArrayList<>();
+        List<String> permissionsNotGranted = new ArrayList<>();  // aceitamos uma lista de permissões como entrada
 
-        for (String permission : permissions) {
-            if (!hasPermission(permission)) {
-                permissionsNotGranted.add(permission); // Adiciona à lista as permissões não concedidas
+        for (String permission : permissions) { // vamos verificar cada permissao
+            if (!hasPermission(permission)) { // caso nao tenha sido confirmada
+                permissionsNotGranted.add(permission); // sera guarda em uma lista que mantem apenas as desse tipo
             }
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (permissionsNotGranted.size() > 0) {
-                // Solicita as permissões não concedidas
                 requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
             }
-            else{
-                MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
-                int navigationOpSelected =vm.getNavigationOpSelected();
-                bottomNavigationView.setSelectedItemId(navigationOpSelected);
-            }
         }
+        //se o app ja tiver todas as informacoes que precisa,acessamos o mainviewmodel (leitura das fotos)
+        else {
+            //obtendo a opcao escolhida pelo usuario
+            MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+            int navigationOpSelected = vm.getNavigationOpSelected();
+            //setando essa opçao.
+            bottomNavigationView.setSelectedItemId(navigationOpSelected);
+        }
+
     }
 
-    // Método para verificar se uma permissão foi concedida
+    // metodo que verifica se uma determinada permissão já foi concedida ou nao
     private boolean hasPermission(String permission) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
@@ -111,43 +130,48 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    // Método que recebe o resultado da solicitação de permissões
+    // esse metodo so sera invocado após o usuário conceder ou não as permissões requisitadas
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         final List<String> permissionsRejected = new ArrayList<>();
         if (requestCode == RESULT_REQUEST_PERMISSION) {
+            // verifica se cada permissao foi concedida pelo usuario ou nao
             for (String permission : permissions) {
                 if (!hasPermission(permission)) {
-                    permissionsRejected.add(permission); // Adiciona à lista as permissões rejeitadas
+                    permissionsRejected.add(permission);
                 }
             }
         }
-
+        // se ha alguma que não foi concedida e eh necessaria, o sistema dispara uma mensagem
         if (permissionsRejected.size() > 0) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
-                    // Mostra um alerta explicando a necessidade das permissões
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setMessage("Para usar esse app é preciso conceder essas permissões")
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // Solicita novamente as permissões rejeitadas
-                                    requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
-                                }
-                            }).create().show();
+
+                if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) // vamos mostrar a mensagem
+                {
+                    new AlertDialog.Builder(MainActivity.this).
+                            setMessage("Para usar essa app é preciso conceder essas permissões"). // mensagem disparada
+                            setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                        // vamos pedir novamente a permissao necessaria para o funcionamento do sistema
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            requestPermissions(permissionsRejected.toArray(new
+                                    String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
+                        }
+                    }).create().show();
                 }
             }
-        }
-        else{
+        } else {
             MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
             int navigationOpSelected = vm.getNavigationOpSelected();
             bottomNavigationView.setSelectedItemId(navigationOpSelected);
         }
     }
-
-
-
 }
+
+
+
+
